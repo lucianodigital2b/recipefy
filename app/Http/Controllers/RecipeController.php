37adfb8e4d5e0febf213bb62908c744c1877cc6d
Luciano <luciano.victor101@outlook.com>
2024-12-20 16:51:17 -0300

    added reciperesource to listing

--- app/Http/Controllers/RecipeController.php
+++ app/Http/Controllers/RecipeController.php
@@ -11,34 +11,74 @@ class RecipeController extends Controller
 {
     protected $recipeService;
 
+    /**
+     * Inject the RecipeService into the controller.
+     *
+     * @param RecipeService $recipeService
+     */
     public function __construct(RecipeService $recipeService)
     {
         $this->recipeService = $recipeService;
     }
 
+
+    /**
+     * Get all published recipes
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function index()
     {
         $recipes = $this->recipeService->getPublishedRecipes();
-        return response()->json($recipes);
+        return RecipeResource::collection($recipes);
     }
+
+    /**
+     * Display the specified recipe.
+     *
+     * @param int $id
+     * @return \Illuminate\Http\JsonResponse
+     */
 
     public function show($id)
     {
         $recipe = $this->recipeService->getRecipeById($id);
-        return response()->json($recipe);
+        return response()->json(RecipeResource::make($recipe));
     }
 
+    /**
+     * Create a new recipe
+     *
+     * @param StoreRecipeRequest $request
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function store(StoreRecipeRequest  $request)
     {
         $recipe = $this->recipeService->createRecipe($request->all());
         return response()->json($recipe, 201);
     }
 
+
+
+    /**
+     * Update an existing recipe.
+     *
+     * @param UpdateRecipeRequest $request
+     * @param int $id
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function update(UpdateRecipeRequest  $request, $id)
     {
         $recipe = $this->recipeService->updateRecipe($id, $request->all());
         return response()->json($recipe);
     }
+
+    /**
+     * Delete a recipe.
+     *
+     * @param int $id
+     * @return \Illuminate\Http\JsonResponse
+     */
 
     public function destroy($id)
     {
